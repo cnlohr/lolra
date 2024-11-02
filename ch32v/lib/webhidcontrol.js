c7900d08b0cb04ea10887a2d164538c474bc54d8
cnlohr <lohr85@gmail.com>
2024-11-02 02:05:13 -0400

    Add licenses and readme to lib files in ch32v

--- ch32v/lib/webhidcontrol.js
+++ ch32v/lib/webhidcontrol.js
@@ -1,3 +1,15 @@
+/* This file is under the regular MIT license. 
+
+Copyright 2024 Charles Lohr (cnlohr)
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the “Software”), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+*/
+
 const expectedProductName = "CNLohr lolra ch32v203 goertzel test";
 const filter = { vendorId : 0x1209, productId : 0xd035 };
 let dev = null;
